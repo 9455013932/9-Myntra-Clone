0c1e70b943c61fd6b16245ebbc3d13cc8dec7d91
9455013932 <kumarsinghdeepak659@gmail.com>
2024-10-07 22:58:29 +0530

    ok

--- 3-myntra-react-clone/src/components/Fetchitems.jsx
+++ 3-myntra-react-clone/src/components/Fetchitems.jsx
@@ -14,7 +14,7 @@ const FetchItems = () => {
     const signal = controller.signal;
 
     dispatch(fetchStatusActions.markFetchingStarted());
-    fetch("https://9-myntra-clone-pi.vercel.app//items", { signal })
+    fetch("https://9-myntra-clone-pi.vercel.app/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchDone());
