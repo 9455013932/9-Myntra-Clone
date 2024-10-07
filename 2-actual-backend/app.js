b06d450f42886a7b8fabf6edd7a871b1eb283f6f
9455013932 <kumarsinghdeepak659@gmail.com>
2024-10-07 23:10:09 +0530

    ok

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -1,41 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
 const { getStoredItems, storeItems } = require('./data/items');
 
-const app = express();
-
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
-app.get('/items', async (req, res) => {
-  const storedItems = await getStoredItems();
-  await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
-  res.json({ items: storedItems });
-});
-
-app.get('/items/:id', async (req, res) => {
-  const storedItems = await getStoredItems();
-  const item = storedItems.find((item) => item.id === req.params.id);
-  res.json({ item });
-});
-
-app.post('/items', async (req, res) => {
-  const existingItems = await getStoredItems();
-  const itemData = req.body;
-  const newItem = {
-    ...itemData,
-    id: Math.random().toString(),
-  };
-  const updatedItems = [newItem, ...existingItems];
-  await storeItems(updatedItems);
-  res.status(201).json({ message: 'Stored new item.', item: newItem });
-});
-
-app.listen(`https://9-myntra-clone-pi.vercel.app/`);
+  if (req.method === 'GET') {
+    if (req.query.id) {
+      const storedItems = await getStoredItems();
+      const item = storedItems.find((item) => item.id === req.query.id);
+      return res.json({ item });
+    } else {
+      const storedItems = await getStoredItems();
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return res.json({ items: storedItems });
+    }
+  } else if (req.method === 'POST') {
+    const existingItems = await getStoredItems();
+    const itemData = req.body;
+    const newItem = {
+      ...itemData,
+      id: Math.random().toString(),
+    };
+    const updatedItems = [newItem, ...existingItems];
+    await storeItems(updatedItems);
+    return res.status(201).json({ message: 'Stored new item.', item: newItem });
+  } else {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
